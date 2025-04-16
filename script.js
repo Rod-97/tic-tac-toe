c70528d3743202ea84eb48d7fda863d9c2862070
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-04-16 19:26:20 +0200

    create board

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("test");
+function Cell() {
+  let value = 0;
+  const addToken = (player) => (value = player);
+  const getValue = () => value;
+  return { addToken, getValue };
+}
+
+function Gameboard() {
+  const board = [];
+  const rows = 3;
+  const columns = 3;
+
+  for (let x = 0; x < rows; x++) {
+    board[x] = [];
+    for (let y = 0; y < columns; y++) {
+      board[x].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+  return { getBoard };
+}
+
+const gameboard = Gameboard();
+console.log(gameboard.getBoard());
