3c4c02c5d16d13c71353110f0ea5e7af9689612a
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-04-21 16:28:54 +0200

    game logic done

--- script.js
+++ script.js
@@ -83,6 +83,15 @@ function anyWinner(board) {
   return checkAllRows() || checkAllColumns() || checkDiagonals();
 }
 
+function isBoardFull(board) {
+  for (let x = 0; x < board.length; x++) {
+    for (let y = 0; y < board.length; y++) {
+      if (board[x][y].getValue() === "") return false;
+    }
+  }
+  return true;
+}
+
 function displayBoard(boardArg) {
   const board = document.createElement("div");
   board.classList.add("board");
@@ -91,8 +100,7 @@ function displayBoard(boardArg) {
     for (let col = 0; col < boardArg.length; col++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
-      if (row % 2 === 0) cell.textContent = "X";
-      else cell.textContent = "O";
+      cell.classList.add(`${row}-${col}`);
       board.appendChild(cell);
     }
   }
@@ -103,25 +111,51 @@ function displayBoard(boardArg) {
 function GameController() {
   const gameboard = Gameboard();
   const board = gameboard.getBoard();
+  const player1 = "X";
+  const player2 = "O";
+  let currentPlayer = player1;
+  displayBoard(board);
 
-  const playRound = (row, col, playerToken) => {
-    const cell = board[row][col];
-    if (cell.getValue() !== "") return;
-    cell.addToken(playerToken);
+  const play = () => {
+    const playRound = (row, col) => {
+      const cell = board[row][col];
+      if (cell.getValue() !== "") return;
+      cell.addToken(currentPlayer);
+      const cellUI = document.getElementsByClassName(`${row}-${col}`)[0];
+      cellUI.textContent = currentPlayer;
+
+      if (anyWinner(board) || isBoardFull(board)) {
+        const winnerDiv = document.createElement("div");
+        winnerDiv.classList.add("winner-div");
+        if (anyWinner(board)) {
+          winnerDiv.textContent = `Player ${currentPlayer} won!`;
+        }
+        if (!anyWinner(board) && isBoardFull(board)) {
+          winnerDiv.textContent = "It's a tie!";
+        }
+        document.body.appendChild(winnerDiv);
+        return;
+      }
+
+      if (currentPlayer === player1) currentPlayer = player2;
+      else currentPlayer = player1;
+    };
+
+    const cells = document.querySelectorAll(".cell");
+
+    cells.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        if (anyWinner(board) || cell.textContent !== "") return;
+        const cellPosition = cell.classList[1].split("-");
+        const row = cellPosition[0];
+        const col = cellPosition[1];
+        playRound(row, col);
+      });
+    });
   };
 
-  return { playRound, board };
+  return { play };
 }
 
 const game = GameController();
-const board = game.board;
-displayBoard(board);
-game.playRound(1, 2, "X");
-game.playRound(0, 2, "O");
-console.log(anyWinner(board)); // false
-game.playRound(2, 2, "X");
-game.playRound(1, 1, "O");
-console.log(anyWinner(board)); // false
-game.playRound(0, 0, "X");
-game.playRound(2, 0, "O");
-console.log(anyWinner(board)); // true
+game.play();
